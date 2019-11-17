ae08768710736f679d0fbce8ee8b9580f5b50295
ask1208 <t.ask.128@icloud.com>
2019-11-17 19:04:04 +0900

    変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  include CarrierWave::MiniMagick
 
   process resize_to_fit: [800, 800]
 
