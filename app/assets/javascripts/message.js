7c3e15793eefd3e3639338bf9d8593cf48be0d93
ask1208 <t.ask.128@icloud.com>
2019-11-23 21:59:32 +0900

    非同期通信の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,50 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<div class="message">
+    <div class="upper-message">
+    <div class="upper-message__user-name">
+    ${message.name}
+    </div>
+    <div class="upper-message__date">
+    ${message.date}
+    </div>
+    </div>
+    <div class="lower-meesage">
+    <div class="lower-message__content">
+    ${message.content}
+
+    <img src=${message.image}>
+    
+    </div>
+    </div>
+    </div>`
+    return html;
+  }
+
+
+
   $('#new_message').on('submit', function(e){
-    e.preventDefault()
+    e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
-    
     $.ajax({
       url: url,  
-      type: 'POST', 
+      type: "POST", 
       data: formData,  
       dataType: 'json',
       processData: false,
-      contentType: false
+      contentType: false,
+    })
+    .done(function(message){
+       var html = buildHTML(message);
+       $('.messages').append(html)
+       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+       $('.box').animate({'height' : '200px'});
+       $('#message_content').val('')
+      })
+    .fail(function(){
+
+
     })
   })
 });
